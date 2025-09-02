45a3d7048dee4806e5b4cc5d85d113d63a8b4ce0
cake <code@cakey.me>
2025-09-02 22:15:01 +0200

    second push - refactor

---
+++ frontend/src/main.rs
@@ -0,0 +1,23 @@
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Hero {}
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+    }
+}
