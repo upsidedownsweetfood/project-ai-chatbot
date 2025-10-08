fb154f4d911a615056f0e007a88cd960bdea14e8
francesco.dibella@thelevelgroup.com <francesco.dibella@thelevelgroup.com>
2025-10-08 09:06:48 +0200

    leptos template

--- leptos-frontend/src/main.rs
+++ leptos-frontend/src/main.rs
@@ -1,20 +1,14 @@
 use leptos::prelude::*;
+use leptos_frontend::App;
 
 fn main() {
+    // set up logging
+    _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(App);
-}
 
-#[component]
-fn App() -> impl IntoView {
-	let (count, set_count) = signal(0);
-
-	view! {
-		<button on:click=move |_| *set_count.write() += 1>
-			"click me: " {count}
-		</button>
-		<p>
-			"Double count: " {move || count.get() * 2}
-		</p>
-	}
+    mount_to_body(|| {
+        view! {
+            <App />
+        }
+    })
 }
