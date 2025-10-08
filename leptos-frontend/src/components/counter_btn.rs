e1acbcc36c74ae69b84b29d4328cbda1066487da
francesco.dibella@thelevelgroup.com <francesco.dibella@thelevelgroup.com>
2025-10-08 09:34:30 +0200

    .

--- leptos-frontend/src/components/counter_btn.rs
+++ leptos-frontend/src/components/counter_btn.rs
@@ -7,7 +7,9 @@ pub fn Button(#[prop(default = 1)] increment: i32) -> impl IntoView {
     view! {
         <button on:click=move |_| {
             set_count(count() + increment)
-        }>
+        }
+        class=(["button-20", "rounded"], move || count.get() % 2 == 1)
+        >
 
             "Click me: " {count}
         </button>
